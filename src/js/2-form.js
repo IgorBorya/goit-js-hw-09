023e471783318ef8d25737c905a0d4811850bf93
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-08-06 14:44:49 +0300

    sixversion

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,38 @@
-const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
-const STORAGE_KEY = 'feedback-form-state';
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const STORAGE_KEY = 'feedback-form-state';
 
-let formData = {
-  email: '',
-  message: '',
-};
+  let formData = {
+    email: '',
+    message: '',
+  };
 
-window.addEventListener('DOMContentLoaded', event => {
   const savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
     formData = JSON.parse(savedData);
     emailInput.value = formData.email;
     messageInput.value = formData.message;
   }
-});
 
-form.addEventListener('input', event => {
-  formData[event.target.name] = event.target.value.trim();
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-});
+  form.addEventListener('input', event => {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  });
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+  form.addEventListener('submit', event => {
+    event.preventDefault();
 
-  if (formData.email === '' || formData.message === '') {
-    alert('Fill please all fields');
-    return;
-  }
+    if (formData.email === '' || formData.message === '') {
+      alert('Fill please all fields');
+      return;
+    }
 
-  console.log(formData);
+    console.log(formData);
 
-  form.reset();
-  localStorage.removeItem(STORAGE_KEY);
-  formData = { email: '', message: '' };
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = { email: '', message: '' };
+  });
 });
